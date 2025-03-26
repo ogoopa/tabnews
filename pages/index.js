4bcde2bda563c4ecff208f69518972849a43fcf9
Gustavo de Matos Parizi <gusparizi@gmail.com>
2025-03-26 18:22:14 -0300

    testing updating content

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Os lábios da sabedoria estão fechados, exceto aos ouvidos do entendimento.
-      🌎
-    </h1>
-  );
+  return <h1>errare humanum est. 🌎</h1>;
 }
 
 export default Home;
