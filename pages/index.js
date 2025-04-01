bb54f73e6454116a81a158b8dc7bef12ba7597c2
Gustavo de Matos Parizi <gusparizi@gmail.com>
2025-04-01 20:30:23 -0300

    updating text

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 function Home() {
   return (
     <h1>
-      "In a world of noise, I create intelligent clarity — through code, AI, and
-      a bit of magic."
+      "At the intersection of logic and wonder, I build technologies that think
+      and feel."
     </h1>
   );
 }
